2b1289b7ff49f6065fa99fa08d765e492cb14ab5
Santos1109 <santos1109@unitec.edu>
2017-09-14 07:06:38 -0600

    Tarea

---
+++ cmake-build-debug/CMakeFiles/Examen2Dragon_master.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/saint/Desktop/Examen2Dragon-master/Evaluador.cpp" "C:/Users/saint/Desktop/Examen2Dragon-master/cmake-build-debug/CMakeFiles/Examen2Dragon_master.dir/Evaluador.cpp.obj"
+  "C:/Users/saint/Desktop/Examen2Dragon-master/NodoNario.cpp" "C:/Users/saint/Desktop/Examen2Dragon-master/cmake-build-debug/CMakeFiles/Examen2Dragon_master.dir/NodoNario.cpp.obj"
+  "C:/Users/saint/Desktop/Examen2Dragon-master/Pais.cpp" "C:/Users/saint/Desktop/Examen2Dragon-master/cmake-build-debug/CMakeFiles/Examen2Dragon_master.dir/Pais.cpp.obj"
+  "C:/Users/saint/Desktop/Examen2Dragon-master/main.cpp" "C:/Users/saint/Desktop/Examen2Dragon-master/cmake-build-debug/CMakeFiles/Examen2Dragon_master.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Examen2Dragon_master.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Examen2Dragon_master.dir/Evaluador.cpp.obj"
+  "CMakeFiles/Examen2Dragon_master.dir/main.cpp.obj"
+  "CMakeFiles/Examen2Dragon_master.dir/NodoNario.cpp.obj"
+  "CMakeFiles/Examen2Dragon_master.dir/Pais.cpp.obj"
+  "Examen2Dragon_master.pdb"
+  "Examen2Dragon_master.exe"
+  "Examen2Dragon_master.exe.manifest"
+  "libExamen2Dragon_master.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Examen2Dragon_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
